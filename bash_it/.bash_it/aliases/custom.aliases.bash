a9f91d01fb41c68b9c7e9f92b1dad95613bdd706
Maksim Selivanau <maksim_selivanau@epam.com>
2022-05-20 20:15:35 +0300

    Add bash_it custom aliases file

---
+++ bash_it/.bash_it/aliases/custom.aliases.bash
@@ -0,0 +1,19 @@
+export REPOSITORY_ROOT=~/repo
+
+# Google Cloud Commands
+alias bq=bq
+alias bqss='bq show --format prettyjson'
+# Custom git aliases
+alias grls='gco master && gpl && gco release && gm master && gp'
+
+# Project DKPI aliases
+export PRJ_DKPI_ROOT="${REPOSITORY_ROOT}/bayc-alec"
+export DKPI_PROD_PROJECT="bayer-ch-230109"
+export DKPI_DEV_PROJECT="bayer-ch-global-dev"
+alias cddk='cd ${PRJ_DKPI_ROOT}'
+alias dkroot="cd ${PRJ_DKPI_ROOT}"
+alias dkchg="cd ${PRJ_DKPI_ROOT}/dags/schema/changelog/"
+alias dkwai="gcloud config get-value project"
+alias dkprod="gcloud config set project ${DKPI_PROD_PROJECT}"
+alias dkdev="gcloud config set project ${DKPI_DEV_PROJECT}"
+alias dklint="pylint --rcfile=${PRJ_DKPI_ROOT}/pylintrc ${PRJ_DKPI_ROOT}/src/digital_kpi" 
