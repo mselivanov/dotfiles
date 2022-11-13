8388f4aeec36e326df51f089bd6f20fe0a9f675b
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-13 22:18:26 +0200

    Change plugin management
    - Use Plugged instead of Vundle
    - Add Coc extensions installation

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 stow bash tmux vim starship
-vim +PluginInstall +qall
+vim +'PlugInstall --sync' +qall
+vim +'CocInstall -sync coc-prettier coc-json coc-pyright coc-git coc-spell-checker' +qall
 stow nvim
