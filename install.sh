a223b96a0993fc1a5252d0b5d821977c481080e1
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-14 00:38:13 +0200

    Add automatic plugin installation on vim/nvim start

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 stow bash tmux vim starship
-vim +'PlugInstall --sync' +qall
+vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
 vim +'CocInstall -sync coc-prettier coc-json coc-pyright coc-git coc-spell-checker' +qall
 stow nvim
