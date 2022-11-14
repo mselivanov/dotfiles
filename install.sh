e81170a997e9cfb9f8a34b4daf287124e79dd8c4
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-14 09:39:42 +0200

    Update vimrc
    - Ignore errors for setting nord theme

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-stow bash tmux vim starship
+stow bash tmux starship vim
 vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
 vim +'CocInstall -sync coc-prettier coc-json coc-pyright coc-git coc-spell-checker' +qall
-stow nvim
+stow nvim 
