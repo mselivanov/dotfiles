69abee2e972f62c70b8460063ac7750da1cffefe
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-15 21:16:17 +0200

    Add plugin installation

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 stow bash tmux starship vim
+vim +'PlugInstall --sync' +qall
 vim +'CocInstall -sync coc-prettier coc-json coc-pyright coc-git coc-spell-checker' +qall
 stow nvim 
