3b44c50f08ebac65ce96298c01ddf5392ed6ead9
Maksim Selivanau <mselivanov@gmail.com>
2022-12-27 22:36:46 +0200

    Update neovim config using neovim from scratch series

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,25 +1,70 @@
------------------------------------------------
--- Editing settings
------------------------------------------------
--- Copy to clipboard
-vim.keymap.set({'n', 'x'}, 'cc', '"+y')
--- Paste from clipboard
-vim.keymap.set({'n', 'x'}, 'cv', '"+p')
+local opts = { noremap = true, silent = true }
 
------------------------------------------------
--- Tab settings
------------------------------------------------
-vim.keymap.set({'n'}, '<C-t>', ':tabnew<CR>')
-vim.keymap.set({'n'}, '<C-F4>', ':tabnew<CR>')
-vim.keymap.set({'n'}, '<C-F4>', ':tabclose<CR>')
-vim.keymap.set({'n'}, '<A-l>', ':tabnext<CR>')
-vim.keymap.set({'n'}, '<A-h>', ':tabprev<CR>')
+local term_opts = { silent = true }
 
------------------------------------------------
--- Window settings
------------------------------------------------
-vim.keymap.set({'n'}, '<C-N>', '<C-W>v')
-vim.keymap.set({'n'}, '<C-J>', '<C-W>j')
-vim.keymap.set({'n'}, '<C-K>', '<C-W>k')
-vim.keymap.set({'n'}, '<C-H>', '<C-W>h')
-vim.keymap.set({'n'}, '<C-L>', '<C-W>l')
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Insert --
+-- Press jk fast to exit insert mode 
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+-- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+-- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+-- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+-- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
