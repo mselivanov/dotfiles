e2ff2bd3ef2f6fac971926cadabf37229ac75a90
Maksim Selivanau <mselivanov@gmail.com>
2023-05-21 22:41:15 +0300

    feat: update alias definition

--- bash_it/.bash_it/aliases/custom.aliases.bash
+++ bash_it/.bash_it/aliases/custom.aliases.bash
@@ -10,8 +10,20 @@ alias gblsr='git branch --list --remotes'
 alias gfd='git fetch origin dev:dev'
 
 # CLI tools aliases
-alias fd='fdfind'
-alias cat='batcat'
+if [[ "x$(which fdfind)" != "x" ]]; then
+	alias fd='fdfind'
+else
+	alias fd='find'
+fi
+
+if [[ "x$(which batcat)" != "x" ]]; then
+	alias cat='batcat'
+elif
+	[[ "x$(which bat)" != "x" ]]
+	alias cat='bat'
+then
+	alias cat='cat'
+fi
 # Remove alias for lf from bash_it
 unalias lf
 
