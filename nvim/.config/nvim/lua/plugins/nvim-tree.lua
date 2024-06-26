01ad6f22bb8048d31674b4dcc947f8f4f5247a7f
Maksim Selivanau <mselivanov@gmail.com>
2024-06-26 19:49:58 +0300

    Update bashrc

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,30 +1,28 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  cmd = { "NvimTreeToggle" },
-  keys = {
-    { "<leader>fe", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" },
-  },
-  opts = {
-    disable_netrw = false,
-    hijack_netrw = true,
-    respect_buf_cwd = true,
-    view = {
-      number = true,
-      relativenumber = true,
-      width = 40
-    },
-    filters = {
-      custom = { ".git" },
-    },
-    sync_root_with_cwd = true,
-    update_focused_file = {
-      enable = true,
-      update_root = true,
-    },
-    actions = {
-      open_file = {
-        quit_on_open = false,
-      },
-    },
-  },
+	"nvim-tree/nvim-tree.lua",
+	cmd = { "NvimTreeToggle" },
+	keys = {
+		{ "<leader>fe", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" },
+	},
+	opts = {
+		disable_netrw = false,
+		hijack_netrw = true,
+		respect_buf_cwd = true,
+		view = {
+			number = true,
+			relativenumber = true,
+			width = 40,
+		},
+		filters = {},
+		sync_root_with_cwd = true,
+		update_focused_file = {
+			enable = true,
+			update_root = true,
+		},
+		actions = {
+			open_file = {
+				quit_on_open = false,
+			},
+		},
+	},
 }
