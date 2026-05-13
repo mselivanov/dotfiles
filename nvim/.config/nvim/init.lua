69fea64c2f83fac761bcb1a863269f497b7fdbe0
Maksim Selivanau <mselivanov@gmail.com>
2026-05-13 15:28:50 +0200

    Fix clipboard for WSL

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,6 +8,18 @@ require("plugins")
 require("config.lsp")
 
 vim.lsp.inline_completion.enable(false)
+vim.g.clipboard = {
+	name = "win32yank-wsl",
+	copy = {
+		["+"] = { "win32yank.exe", "-i", "--crlf" },
+		["*"] = { "win32yank.exe", "-i", "--crlf" },
+	},
+	paste = {
+		["+"] = { "win32yank.exe", "-o", "--lf" },
+		["*"] = { "win32yank.exe", "-o", "--lf" },
+	},
+	cache_enabled = 0,
+}
 
 pcall(function()
 	require("local")
